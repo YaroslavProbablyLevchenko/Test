8605e38eabd1a4aa91673cdce8690341c61ad588
Yaroslav <yaroslavgetback@gmail.com>
2018-01-23 03:33:19 +0200

    Добавление модели Учитель

---
+++ Learn/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn.Models
+{
+    public class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
