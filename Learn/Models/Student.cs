f9aaf540f6d4807b9cacd8a40ec58f1bf5319e2f
Yaroslav <yaroslavgetback@gmail.com>
2018-01-23 03:28:07 +0200

    Добавление модели студент

---
+++ Learn/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    
+
+    }
+}
